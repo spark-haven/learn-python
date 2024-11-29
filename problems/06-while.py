5efd2305ea75d1c4ff8615a9767f072a416417a4
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-29 21:52:55 +0530

    Added problems/06-while.py

---
+++ problems/06-while.py
@@ -0,0 +1,2 @@
+# Make a while loop which increases the count of hour by 1 until it reaches 24. then it has to exit telling the end of the day reached
+# In python x = x + 1 can be written as x += 1 same with other operators
