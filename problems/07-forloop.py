13f23bb9d56d34ea33178d3519d758c36928bde7
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-29 21:52:55 +0530

    Added problems/07-forloop.py

---
+++ problems/07-forloop.py
@@ -0,0 +1,6 @@
+# Make a forloop which spams the console with the numbers spammer {1} to {10}
+#like 
+# spammer 1
+# spammer 2
+# etc.... 
+# Wait did you get an error of wrong datatype yes?? please use google to fix it yourself
