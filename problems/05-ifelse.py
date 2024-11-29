69e870b894a087d45a93b8c7399d52cf4f909d8f
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-29 21:52:55 +0530

    Added problems/05-ifelse.py

---
+++ problems/05-ifelse.py
@@ -0,0 +1,3 @@
+# make an if else statement which can tell good morning,good afternoon and good evening according to the hour given in a variable also if you want you can use:
+hour = input("Enter the current hour: ")
+#Ask the user the hour
