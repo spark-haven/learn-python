63b75c81470537d0309c34657deff8d6ce40f4a8
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-29 21:52:55 +0530

    Added concepts/05-elif.py

---
+++ concepts/05-elif.py
@@ -0,0 +1,15 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("Don't forget to wear sunscreen!")
+elif weather == "rainy":
+    print("Don't forget to bring an umbrella!")
+else:
+    print("Don't forget to bring a jacket!")
+
+# == is a comparison operator that checks if two values are equal
+# != is a comparison operator that checks if two values are not equal
+# > is a comparison operator that checks if the value on the left is greater than the value on the right
+# < is a comparison operator that checks if the value on the left is less than the value on the right
+# >= is a comparison operator that checks if the value on the left is greater than or equal to the value on the right
+# <= is a comparison operator that checks if the value on the left is less than or equal to the value on the right
