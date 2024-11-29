27f05dd52a9055256617bd9fabca86a35fcfedf8
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-29 21:52:55 +0530

    Added solutions/08-functions.py

---
+++ solutions/08-functions.py
@@ -0,0 +1,12 @@
+def vowels1(text):
+    for letter in text:
+        if letter in "aeiou":
+            print(letter)
+
+def vowels2(text):
+    for letter in text:
+        if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+            print(letter)
+
+vowels1("hello")
+vowelsl("hello")
