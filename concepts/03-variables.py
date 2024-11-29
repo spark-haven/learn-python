9d03bebf1c39963c3cc6b57e601b3c3f6c28797e
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-29 21:52:54 +0530

    Added concepts/03-variables.py

---
+++ concepts/03-variables.py
@@ -0,0 +1,10 @@
+# variables can store information
+name = "Rohit" # string
+age = 14 # integer
+gpa = 4.0 # float
+programmer = True # boolean
+
+# python gives its variables typing automatically in some other programming languages we have to assign it ourselves like string name = "Rohit" or int age = 14
+
+
+
