eb6a4bf8dca07a1e2e7af58bbcc1dde92219c424
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-29 21:52:55 +0530

    Added concepts/06-datacol.py

---
+++ concepts/06-datacol.py
@@ -0,0 +1,23 @@
+#List is ordered,mutable,collection of items
+fruits = ["apple", "banana", "cherry"]  )
+fruits.append("orange")  
+print(fruits) 
+
+#Tuple is ordered,immutable,collection of items
+coordinates = (10, 20) 
+print(coordinates[0])  
+
+#Set is unordered,unindexed,collection of items
+colors = {"red", "blue", "green"}  
+colors.add("yellow")  
+print(colors)  
+
+#Dictionary is unordered,changeable,indexed,collection of items
+person = {"name": "Alice", "age": 30, "city": "New York"}  
+print(person["name"])  
+person["age"] = 31  
+print(person)  
+
+
+
+
